70ae7b4e3d108ffc1f7f1c00cbf7cdae8b8eab39
GrodyJenner <91395613+GrodyJenner@users.noreply.github.com>
2021-09-25 19:16:39 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "https://sleeper.app/leagues/649911979372281856"; // your league ID
+export const leagueID = "649911979372281856"; // your league ID
 export const leagueName = "The League of Oofs"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
